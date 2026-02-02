5888e35014740ace57cc82582b5dfdfb8e2ebeb5
Aman-Yadav03 <amanyadav0542vns@gmail.com>
2026-02-02 19:38:54 +0530

    Api fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+const API_URL = `${process.env.REACT_APP_BASE_URL}/api/chat`;
 
 export async function sendMessageToBot(message) {
-  const response = await axios.post(`${API_URL}/api/chat`, {
+  const response = await axios.post(API_URL, {
     message,
   });
 
